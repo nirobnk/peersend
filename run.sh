78e11884de9670c185620978e29d2871dc3562a4
nirobnk <eg245158@engug.ruh.ac.lk>
2026-02-23 16:17:52 +0530

    peersend os project

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# PeerSend Launcher Script
-
-# Get the directory where this script is located
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Run the application using Maven (most reliable method)
-cd "$DIR"
-mvn javafx:run
